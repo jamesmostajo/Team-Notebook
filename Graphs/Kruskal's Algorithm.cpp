303a12ec0262a1254014f32ce0336f4ee41e7af7
Arthur Lluisma <ajclluisma@gmail.com>
2026-03-15 05:04:24 +0800

    add optimized kruskal's

--- Graphs/Kruskal's Algorithm.cpp
+++ Graphs/Kruskal's Algorithm.cpp
@@ -1,3 +1,28 @@
+vector<int> parent, rank;
+
+void make_set(int v) {
+    parent[v] = v;
+    rank[v] = 0;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}
+
 struct Edge {
     int u, v, weight;
     bool operator<(Edge const& other) {
@@ -9,20 +34,18 @@ int n;
 vector<Edge> edges;
 
 int cost = 0;
-vector<int> tree_id(n);
 vector<Edge> result;
+parent.resize(n);
+rank.resize(n);
 for (int i = 0; i < n; i++)
-    tree_id[i] = i;
+    make_set(i);
 
 sort(edges.begin(), edges.end());
 
 for (Edge e : edges) {
-    if (tree_id[e.u] != tree_id[e.v]) {
+    if (find_set(e.u) != find_set(e.v)) {
         cost += e.weight;
         result.push_back(e);
-
-        int old_id = tree_id[e.u], new_id = tree_id[e.v];
-        for (int i = 0; i < n; i++) {
-            if (tree_id[i] == old_id)
-                tree_id[i] = new_id;
-}}}
+        union_sets(e.u, e.v);
+    }
+}
